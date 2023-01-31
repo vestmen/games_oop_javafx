5fbc45d40b09503651c043faa9699547208b7eb6
AlexNemtsev <alex.nemtsev95@gmail.com>
2023-01-31 21:28:30 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -23,7 +23,7 @@ public final class Logic {
     private boolean free(Cell[] steps) throws OccupiedCellException {
         for (Figure figure : figures) {
             for (Cell step : steps) {
-                if (figure.position() == step) {
+                if (figure.position().equals(step)) {
                     throw new OccupiedCellException("Cell is occupied");
                 }
             }
